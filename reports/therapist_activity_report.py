e213c03798b17744baa9b77147a0ed32fa0004f1
chensz <chensz.hengki@gmail.com>
2026-05-02 21:28:42 +0700

    fix bug report

--- reports/therapist_activity_report.py
+++ reports/therapist_activity_report.py
@@ -109,7 +109,7 @@ class TherapistActivityAbstract(models.AbstractModel):
             WHERE
                 DATE(ss.start_time AT TIME ZONE 'UTC' AT TIME ZONE 'Asia/Jakarta') >= %(date_from)s
                 AND DATE(ss.start_time AT TIME ZONE 'UTC' AT TIME ZONE 'Asia/Jakarta') <= %(date_to)s
-                AND so.state in ('confirm', 'done)
+                AND so.state in ('confirm', 'done')
                 AND ss.state = 'done'
             GROUP BY
                 ss.therapist_id,
